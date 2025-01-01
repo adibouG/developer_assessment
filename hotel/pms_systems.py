02216f41f775150a448070694637e514e8a61f55
adibouG <ad.gonzalezoliva@gmail.com>
2025-01-01 23:06:47 +0100

    implementation

--- hotel/pms_systems.py
+++ hotel/pms_systems.py
@@ -1,10 +1,14 @@
 from abc import ABC, abstractmethod
 import inspect
 import sys
-import json
-import uuid
+import re
+from json import loads, JSONDecodeError
+from uuid import UUID
+from time import sleep
+from datetime import datetime
 
-from typing import Optional, Type, TypedDict
+
+from typing import Any, Callable, Optional, Type, TypedDict
 
 from hotel.external_api import (
     get_reservations_for_given_checkin_date,
@@ -13,7 +17,7 @@ from hotel.external_api import (
     APIError,
 )
 
-from hotel.models import Stay, Hotel
+from hotel.models import Stay, Hotel, Guest, Language
 
 
 class CleanedWebhookPayload(TypedDict):
@@ -64,48 +68,211 @@ class PMS_Apaleo(PMS):
     @classmethod
     def clean_webhook_payload(cls, payload: str) -> Optional[CleanedWebhookPayload]:
         # check for valid payload
-        if payload is None or len(payload) == 0:
-            raise Exception("Invalid or No payload") 
-       # if "HotelId" not in payload:
-       #     raise ValueError("Missing HotelId") 
-        # sanitize and check the payload 
-        #payload = payload.replace("\n", "").replace("\r", "").replace(" ", "") 
-        if payload[0] != "{" or  payload[-1] != "}" : # check that we have something that looks like a Json Object kind 
-            raise Exception("Invalid JSON") 
-        # parsing.... 
-        # could be done with json.load or also an eval but yeah let's do it manually
-            # remove the curly braces from the payload
-        ''''payload = payload.strip('{}')
-        if len(payload) == 0:
-            raise Exception("Empty JSON") 
+        if payload is None or len(payload) <= 0:
+            return None
+        if "HotelId" not in payload:
+            return None
         try:
-            # build the dictionary            
-            pairList =  payload.split(',')  # split the string into key-value pairs
-                # removing the quotes from the keys
-            load = { k[1:-1]: v[1:-1] for k, v in (pair.split(':') for pair in pairList) }
-            
-             or payload["HotelId"] is None or not isinstance(payload["HotelId"], uuid.UUID) or len(payload["HotelId"]) == 0
-            '''
-
-        try:
-            load = json.loads(payload)
-            pms_hotelId = load.get("HotelId") #str(load.get("HotelId"))
             # get the hotel
-            hotelObj = Hotel.objects.get(pms_hotel_id=pms_hotelId) #load.get("HotelId"))  #use and remove the HotelId key/value pair
-            dataDic = load # the rest of the payload is the data
-            print(hotelObj.pms_hotel_id, hotelObj, dataDic)
-            cleanedPayload: CleanedWebhookPayload = dict(hotel_id=hotelObj.id, data=dataDic)
-            print(cleanedPayload)
+            load = loads(payload)
+            pms_hotelId: str = load.get("HotelId")
+            pmsHotelUuid = UUID(pms_hotelId) 
+            hotelId: int = Hotel.objects.filter(pms_hotel_id=pmsHotelUuid).first().id
+            # rebuild the payload as typed data structure
+            cleanedPayload: CleanedWebhookPayload = { 'hotel_id': hotelId, 'data': load }
             return cleanedPayload
-        except SyntaxError:
+        except (JSONDecodeError, KeyError, ValueError, Hotel.DoesNotExist, TypeError, Exception): 
             return None
-        if not isinstance(payload, CleanedWebhookPayload):
-            return None
-        return None
+  
 
     def handle_webhook(self, webhook_data: dict) -> bool:
+        # check for valid payload
+        if webhook_data is None:
+            return False
+        #_____________//PARAMETERS SETTINGS //_____________# 
+        # API calls RETRY and WAIT parameters settings, 
+        RETRY: int = 3
+        WAIT: int = 1 
+        # Phone number validation check
+        DO_PHONE_CHECK: bool = False
+        DUPLICATE_PHONE_SUFFIX: str = "_-1_"
+        #_____________// 
+        try:
+            webhook_data = CleanedWebhookPayload(webhook_data)
+            hotelId: int = webhook_data.get("hotel_id")
+            data: dict = webhook_data.get("data")
+            pmsHotelId: UUID = UUID(data.get("HotelId"))
+            integrationId: UUID = UUID(data.get("IntegrationId"))
+            dataEvents: list = data.get("Events")
+            reservationUpdatesList: list = []
+            # check if the hotel is the right one
+            if hotelId != self.hotel.id:
+                return False
 
-        return False
+            #_______________//Start of fetch reservations details___________
+            # get the reservation updates details as a list
+            # get the reservation details, we could setup a specific pms method returning a list to get reservation details, but this is just an example,       
+            for event in dataEvents:
+                reservationEvent: dict = event
+                eventType: str = reservationEvent.get("Name")
+                eventValue: dict = reservationEvent.get("Value")
+                if eventType != "ReservationUpdated":
+                    continue
+                reservationId: str = eventValue.get("ReservationId")  
+                if reservationId is None or len(reservationId) == 0:
+                    continue                  
+                # get the reservation details 
+                # API call use additional wrapper function named api_call_retry 
+                # it's defined out of the scope of this PMS class, see below 
+                try:
+                    reservationDetails: str =  api_call_retry(get_reservation_details, reservationId, RETRY, WAIT) 
+                    if reservationDetails is not None and len(reservationDetails) > 2 :
+                        print(reservationDetails)
+                        # the data received is from the pms for a specific hotel so this is just a check just in case 
+                        if reservationDetails.find(f'"HotelId": "{pmsHotelId}"') != -1:  
+                            reservationUpdatesList.append(loads(reservationDetails))
+                except APIError as e:
+                    print(f"APIError on reservation: {reservationId}\nError: {e}")
+                        
+                continue
+            # if no reservation updates, we can return
+            if len(reservationUpdatesList) == 0:
+                return True
+            #_______________//End of fetch reservations details___________
+
+            #_______________//Start update guests and stays___________
+            
+            # now we have the list of reservation to creates or updates, we can get the stay and guest details  
+            # mapping of the pms statuses to our supported statuses
+            pmsStayStatusMap: dict = { 
+                "not_confirmed": Stay.Status.UNKNOWN, 
+                "booked": Stay.Status.BEFORE, 
+                "in_house": Stay.Status.INSTAY, 
+                "checked_out": Stay.Status.AFTER,
+                "cancelled": Stay.Status.CANCEL,
+                "no_show": Stay.Status.UNKNOWN
+            }
+            # loop over the reservation updates list and update or create the stays and guests
+            for reservationData in reservationUpdatesList:
+                print(reservationData)
+                try:
+                    pmsReservationId: str = reservationData.get("ReservationId")
+                    # First: get the guest details, we could setup a specific pms method to get and update the guest details, but this is just an example, 
+                    # no need to check guestId as it comes directly from the Pms
+                    guestDetails: str = api_call_retry(get_guest_details, reservationData.get("GuestId"), RETRY, WAIT)
+                    guestObj = loads(guestDetails)
+                    # get the guest phone as this is our system unique identifier and other details
+                    pmsGuestId: str = guestObj.get("GuestId")
+                    guestPhone: str = guestObj.get("Phone")
+                    guestName: str = guestObj.get("Name")
+                    guestLang: str = guestObj.get("Country")
+                    if guestLang is not None and len(guestLang) > 0 and  guestLang.lower() != "null" and \
+                      guestLang.lower() in Language.values:
+                        guestLang = Language(guestLang.lower())
+                    else:
+                        guestLang = None
+                    
+                    if DO_PHONE_CHECK : 
+                        phoneReg = "^\\+?\\d{1,4}?[-.\\s]?\\(?\\d{1,3}?\\)?[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,9}$"
+                    else:
+                        phoneReg = "^.{0,200}$"
+                    
+                    if guestPhone is None or re.fullmatch(phoneReg, guestPhone) is None:
+                        guestPhone = ""
+                    
+                    if guestName is None :
+                        guestName = ""
+                    
+                    # update or create the guest, Note that storing an email in addition to the phone would help to identify the guest 
+                    guestUpdate: dict = { "name": guestName, "phone": guestPhone, "language": guestLang }
+                    print (guestUpdate)
+                    
+                    # if 2 different guest have the same phone number, we should update the guest or the phone number
+                    # but updating the guest might fail with the test cases, 
+                    # so we update the phone number with a suffixe and create or update this new guest
+                    # this could be done in separate function handling the case of phone number update and guest update
+                    doGuestUpdate = True
+                    guestExist = Guest.objects.filter(phone=guestPhone).first()
+                    if guestExist is not None:
+                        if (guestExist.name.strip().lower() != guestName.strip().lower() or \
+                            guestExist.language is None or guestLang is None or guestExist.language != guestLang):
+                            # same phone but  name  or country might differ, so we can't confirm same guest, an email would be nice
+                            guestPhone = guestPhone + DUPLICATE_PHONE_SUFFIX
+                            # either :
+                                # create a new guest to prevent issues with the phone number modified with the suffixe DUPLICATE_PHONE_SUFFIX
+                            guestUpdate["phone"] = guestPhone
+                                # or update the existing guest with the modified phone number 
+                            # guestExist.phone = guestPhone 
+                            # guestExist.save()
+                                #  we could fine tune this decision by checking guest creation and update dates and reservation status to decide which one to update example
+                                # if we have the country we could also decide to add the country code as a PREFIX to the phone number to make it unique 
+                        else:
+                            # else the name and country are the same so we do nothing
+                            doGuestUpdate = False
+                            
+                    # update the guest details
+                    # we still call update_or_create as the modified number might be the same as another guest number,
+                    # but we can't go adding duplicate prefixes so just update the guest with the same prefixed duplicate phone number
+                    # and otherwise create a new guest with the modified phone number         
+                    if doGuestUpdate is True:     
+                        guest, _= Guest.objects.update_or_create(phone=guestPhone, defaults=guestUpdate)
+                        print(f"guest model with id {guest.id} processed")  
+                            
+                except APIError as e:
+                    print(f"APIError on guest: {pmsGuestId}\nError: {e}")
+                    #continue # No guest details should we move to the next reservation?
+                
+                # Now we are sure to have the Guest in our db or an updated guest details  
+                
+                # stay update    
+                # we can update or create the stays accordingly:
+                
+                # NOTE:
+                #   - The only use case I see for using get_reservations_for_given_checkin_date is to notify customers about it 
+                #   or for the edge case of getting the reservations that were booked prior to the pms and hotel 
+                #   integration, and reservationss that didn't get updated since the integration is working
+                #   As a result, we can get guest request for an existing reservation which is not in our system, 
+                #   This might mostly happen with new integrations, but can happen, is it acceptable ? 
+                #       Handling these case would mean get and check all the coming pms reservations of the hotels
+                #       get the missing ones and add these to the db. 
+                #       Not very likely doable in 1 go for all hotels pms for all future dates 
+                #       A compromise could be to set up a time offset value like COMING_DAYS_CHECK = 15 
+                #       and every night run the method get_reservations_for_given_checkin_date over the define time offset
+                #       hotel per hotel or pms per pms, compare our db data with the pms data and add the missing reservations in our db
+                #       
+                # This could be managed in a cron job to run every night, or another process manager or scheduler 
+                # However this has to be implemented in another method or function in order to handle this case
+                
+                #   for now we can assume we are up-to-date with the reservation updates from the pms we just received
+                #   and can update our db stay details accordingly , 
+                
+                # we could setup a specific pms method to  update the stay details, but this is just an example, 
+                # update or create the stay details, no need to check stayId as it comes directly from the Pms
+                try:     
+                    pmsStayStatus: Stay.Status = pmsStayStatusMap.get(reservationData.get("Status").lower(), Stay.Status.UNKNOWN)  
+                    stayStatus: str = Stay.Status(pmsStayStatus)
+                    print(stayStatus)
+
+                    stayUpdate: Stay = { 
+                        'hotel': self.hotel,
+                        'guest': guest, 
+                        'pms_reservation_id': pmsReservationId, 
+                        'pms_guest_id': pmsGuestId,
+                        'status': stayStatus,
+                        'checkin': datetime.strptime(reservationData.get("CheckInDate"), "%Y-%m-%d").date(),
+                        'checkout': datetime.strptime(reservationData.get("CheckOutDate"), "%Y-%m-%d").date(),
+                    } 
+                    stay, created = Stay.objects.update_or_create(pms_reservation_id=pmsReservationId, defaults=stayUpdate)
+                    print(f"stay model with id {stay.id} for reservation {pmsReservationId} processed ") # this will print 'created' or 'updated: {pmsStayId}\nError: {e}")
+                except Exception as e:
+                    print(f"Error on stay update: {pmsReservationId}\nError: {e}")
+                    continue
+            #_______________//End update guests and stays________
+            return True
+
+        except Exception as e:
+            print(f"Error: {e}")
+            return False
 
 
 def get_pms(name: str) -> Type[PMS]:
@@ -125,3 +292,22 @@ def get_pms(name: str) -> Type[PMS]:
         return getattr(current_module, fullname)
     else:
         raise ValueError(f"No PMS class found for {name}")
+
+
+
+# Additional function to handle api call with failing case via retry and sleep parameters... could be an async one       
+def api_call_retry(func: Callable[[Any], Any], param: Any, retry: int = 1, wait: int = 1) -> dict:
+    res = None
+    try:
+        res =  func(param)
+    except APIError as e:
+        print(f"APIError: {e}, retry left: {retry}")
+        sleep(wait)
+        if retry > 0:
+            retry -= 1
+            res = api_call_retry(func, param, retry, wait)
+        else:
+            raise e
+    return res
+
+
--- hotel/tests/tests.py
+++ hotel/tests/tests.py
@@ -5,6 +5,7 @@ from hotel.models import Stay, Hotel, Guest
 from hotel.tests import load_api_fixture
 from hotel.tests.factories import HotelFactory
 
+
 from hotel.pms_systems import CleanedWebhookPayload
 
 
@@ -17,12 +18,20 @@ class PMS_Apaleotest(django.test.TestCase):
         cleaned_payload = self.pms.clean_webhook_payload(load_api_fixture("webhook_payload_faulty.json"))
         self.assertIsNone(cleaned_payload)
 
+
     def test_clean_webhook_payload(self):
         cleaned_payload = self.pms.clean_webhook_payload(load_api_fixture("webhook_payload.json"))
         if not cleaned_payload:
             self.fail("No cleaned payload returned")
         else:
-            self.assertIsInstance(cleaned_payload, CleanedWebhookPayload)
+            # MODIFIED: CleanedWebhookPayload to dict in assertIsInstance method arguments 
+            # Python3.11 (didn't try with other versions) isInstance seems to not work with TypedDict (seems like the mapping to dict is not done by the interpreter anymore), 
+            self.assertIsInstance(cleaned_payload, dict) #CleanedWebhookPayload) 
+            # added 2 manual check for correct dict value types, 
+            # see NOTE in the end of the file for more details about the error raised  when running the tests
+            self.assertIn('hotel_id', cleaned_payload)
+            self.assertIn('data', cleaned_payload)
+            #_______//END OF ADDITION
             self.assertEqual(cleaned_payload["hotel_id"], self.hotel.id)
             self.assertIsInstance(cleaned_payload["data"], dict)
 
@@ -34,3 +43,20 @@ class PMS_Apaleotest(django.test.TestCase):
         self.assertEqual(stays.count(), 3)
         guests = Guest.objects.all()
         self.assertEqual(guests.count(), 3)
+
+
+'''
+NOTE:
+
+ERROR: test_clean_webhook_payload (hotel.tests.tests.PMS_Apaleotest.test_clean_webhook_payload)
+----------------------------------------------------------------------
+Traceback (most recent call last):
+  File "/home/ad/Public/HDD_ext4/Dev2/Python/developer_assessment/hotel/tests/tests.py", line 25, in test_clean_webhook_payload
+    self.assertIsInstance(cleaned_payload, CleanedWebhookPayload)
+  File "/usr/lib/python3.11/unittest/case.py", line 1294, in assertIsInstance
+    if not isinstance(obj, cls):
+           ^^^^^^^^^^^^^^^^^^^^
+  File "/usr/lib/python3.11/typing.py", line 2957, in __subclasscheck__
+    raise TypeError('TypedDict does not support instance and class checks')
+TypeError: TypedDict does not support instance and class checks
+'''
